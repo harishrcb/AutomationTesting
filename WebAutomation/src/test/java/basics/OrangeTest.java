11d6d5e21a1ed513feb777a05cb42bc9b40284eb
kommalapatiharish.k@gmail.com <kommalapatiharish.k@gmail.com>
2026-07-07 02:09:07 +0530

    Test case are added and POM file is updated

---
+++ WebAutomation/src/test/java/basics/OrangeTest.java
@@ -0,0 +1,37 @@
+package basics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class OrangeTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+
+    @Test
+    public void loginTest() {
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+    }
+
+
+    @AfterMethod
+    public void quitBrowser() {
+        driver.quit();
+    }
+}
